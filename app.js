ee59df060ffcafb182bef33bb991e4e64e6ef45f
HigashiNana <ssds.dr.hshow@gmail.com>
2017-03-01 14:27:05 +0900

    chat1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const fs = require('fs');
-const socketio = require('socketio');
+const socketio = require('socket.io');
 
 const port = 3000;
 
-const server = http.createServer(function(req,res,){
-  res.writerHead(200,{'content-type':'text/html'});
-  res.ent(fs.readFileSync(__dirname + '/client/index.html'),);
+const server = http.createServer(function(req,res){
+  res.writeHead(200,{'content-type':'text/html'});
+  res.end(fs.readFileSync(__dirname + '/client/index.html'));
 }).listen(port);
 
 //socketioとの結びつけ
@@ -14,7 +14,8 @@ const io = socketio.listen(server);
 //ioのセットに対してイベントを登録
 io.sockets.on('connection',function(socket){
   console.log(socket);
-  socket.on('massage',function(){
+  socket.on('massage',function(data){
+    console.log(data);
     //飛んできたメッセージのデータを返す
     io.sockets.emit('from_server',{value:data.value});
   });
